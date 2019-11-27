0ab2f114ee9fde9892ae7ec0ae1e93a3e85dfc5e
Swati Choudhari <swati@plenarea.com>
2019-11-27 11:16:44 +0530

    mysql details modified

--- config.php
+++ config.php
@@ -13,12 +13,17 @@ $conn = mysql_connect("localhost","root","root")
  * using mysqli_connect instead
  */
 
-$databaseHost = 'localhost';
+$databaseHost = 'plenareaphpmysql.mysql.database.azure.com';
 $databaseName = 'test';
-$databaseUsername = 'root';
-$databasePassword = 'Pass@word30';
+$databaseUsername = 'swatiplenarea@plenareaphpmysql';
+$databasePassword = 'Temp4now$';
 
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+$con=mysqli_init(); 
+mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
+
+$mysqli = mysqli_real_connect($con, $databaseHost, $databaseUsername, $databasePassword, $databaseName, 3306);
+
+//$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
  if( ! $mysqli) {
     die('No connection: ' . mysqli_connect_error());
 }
